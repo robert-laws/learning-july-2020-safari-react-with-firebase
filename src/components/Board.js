7cf42778fee32de95e482bd7d04f3ef53352ff1c
Robert Laws <roblaws@me.com>
2020-07-19 18:37:36 +0300

    refactor data structure for boards

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,10 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import List from './List';
 
-const Board = ({ board: { id, title, lists } }) => {
+const Board = ({ board: { title, lists } }) => {
   return (
     <div>
       <h4>{title}</h4>
+      <p>{Object.keys(lists).length}</p>
       {/* <div className='list-wrapper'>
         {lists.length &&
           lists.map((list) => <List key={list.id} list={list} />)}
--- src/data/boardData.js
+++ src/data/boardData.js
@@ -3,34 +3,32 @@ export const boardData = [
     id: 100,
     title: 'Programming Ideas',
     background: '#FFAA33',
-    lists: [
-      {
-        id: 1000,
-        title: 'React',
-        board: 100,
-        cards: [
-          {
-            id: 1,
-            text: 'Create components',
-          },
-          {
-            id: 2,
-            text: 'Add header and footer sections',
-          },
-        ],
-      },
-    ],
+    lists: {
+      id: 1000,
+      title: 'React',
+      board: 100,
+      cards: [
+        {
+          id: 1,
+          text: 'Create components',
+        },
+        {
+          id: 2,
+          text: 'Add header and footer sections',
+        },
+      ],
+    },
   },
   {
     id: 200,
     title: 'Home Improvement Plan',
     background: '#80DDAA',
-    lists: [],
+    lists: {},
   },
   {
     id: 300,
     title: 'Vacation Trip Notes',
     background: '#BECC00',
-    lists: [],
+    lists: {},
   },
 ];
